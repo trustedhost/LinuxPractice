975e8ec8e5e424328f9887fba283153b9b17287f
trustedhost <mrchjung@korea.ac.kr>
2024-09-03 15:38:24 +0900

    solve : tcp 서버가 멀티 프로세스로 동작하게 만듦.

--- socket/tcp_server_fork.c
+++ socket/tcp_server_fork.c
@@ -4,16 +4,17 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <sys/wait.h>
 
-#define TCP_PORT 5100 				/* 서버의 포트 번호 */
+#define TCP_PORT 5100  /* 서버의 포트 번호 */
 
 int main(int argc, char **argv)
 {
-    int ssock; 					/* 소켓 디스크립트 정의 */
+    int ssock;  /* 소켓 디스크립트 정의 */
     socklen_t clen;
     pid_t pid;
     int n;
-    struct sockaddr_in servaddr, cliaddr; 	/* 주소 구조체 정의 */
+    struct sockaddr_in servaddr, cliaddr;  /* 주소 구조체 정의 */
     char mesg[BUFSIZ];
 
     /* 서버 소켓 생성 */
@@ -26,50 +27,71 @@ int main(int argc, char **argv)
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(TCP_PORT); 	/* 사용할 포트 지정 */
+    servaddr.sin_port = htons(TCP_PORT);  /* 사용할 포트 지정 */
 
     /* bind 함수를 사용하여 서버 소켓의 주소 설정 */
+    unlink()
     if(bind(ssock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
         perror("bind()");
+        close(ssock);
         return -1;
     }
 
     /* 동시에 접속하는 클라이언트의 처리를 위한 대기 큐를 설정 */
     if(listen(ssock, 8) < 0) {
         perror("listen()");
+        close(ssock);
         return -1;
     }
 
     clen = sizeof(cliaddr);
-    do {
+    while(1) {
         /* 클라이언트가 접속하면 접속을 허용하고 클라이언트 소켓 생성 */
-        int n, csock = accept(ssock, (struct sockaddr *)&cliaddr, &clen);
+        int csock = accept(ssock, (struct sockaddr *)&cliaddr, &clen);
+        if(csock < 0) {
+            perror("accept()");
+            continue;
+        }
 
         /* 네트워크 주소를 문자열로 변경 */
         inet_ntop(AF_INET, &cliaddr.sin_addr, mesg, BUFSIZ);
         printf("Client is connected : %s\n", mesg);
 
         if((pid = fork()) < 0) {
-          perror("Error");
-        } else if(pid == 0) {
-          do {
-            memset(mesg, 0, BUFSIZ);
-            if((n = read(csock, mesg, BUFSIZ)) <= 0)
-              perror("read()");
-            printf("Received data : %s", mesg);
+            perror("fork()");
+            close(csock);
+            continue;
+        } else if(pid == 0) {  /* 자식 프로세스 */
+            close(ssock);  /* 자식 프로세스에서 서버 소켓을 닫음 */
 
-            /* 클라이언트로 buf에 있는 문자열 전송 */
-            if(write(csock, mesg, n) <= 0)
-                perror("write()");
-          } while(strncmp(mesg, "q", 1));
+            while(1) {
+                memset(mesg, 0, BUFSIZ);
+                n = read(csock, mesg, BUFSIZ);
+                if(n <= 0) {
+                    if(n < 0) perror("read()");
+                    break;  /* 클라이언트가 연결을 종료한 경우 루프 종료 */
+                }
 
-          close(csock); 			/* 클라이언트 소켓을 닫음 */
+                printf("Received data : %s", mesg);
 
-          exit(0);
+                /* 클라이언트로 buf에 있는 문자열 전송 */
+                if(write(csock, mesg, n) <= 0) {
+                    perror("write()");
+                    break;
+                }
+
+                if(strncmp(mesg, "q", 1) == 0)
+                    break;
+            }
+
+            close(csock);  /* 클라이언트 소켓을 닫음 */
+            exit(0);  /* 자식 프로세스 종료 */
+        } else {  /* 부모 프로세스 */
+            close(csock);  /* 부모 프로세스에서는 클라이언트 소켓을 닫음 */
+            // waitpid(pid, NULL, 0);  /* 자식 프로세스의 종료를 기다림 */
         }
-    } while(strncmp(mesg, "q", 1));
+    }
 
-    close(ssock); 			/* 서버 소켓을 닫음 */
-
+    close(ssock);  /* 서버 소켓을 닫음 */
     return 0;
 }
