87ecc57a3e383db0efafcd82f1cf8d4ae348d48d
trustedhost <mrchjung@korea.ac.kr>
2024-09-10 10:52:23 +0900

    0909 framebuffer 활용 - mmap 을 활용하면 read/write syscall할 필요 없이 메모리 매핑을 통해서 값 수정을 해서 엄청난 시간 단축이 가능하다.

--- multimedia/framebuffer/fbdraw.c
+++ multimedia/framebuffer/fbdraw.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <linux/fb.h>
 #include <sys/ioctl.h>
+#include <sys/mman.h>
 
 #define FBDEVICE "/dev/fb0"
 
@@ -44,6 +45,33 @@ static void drawface(int fd, int start_x, int start_y, int end_x, int end_y, \
     }
 }
 
+static void drawfacemmap(int fd, int start_x, int start_y, int end_x, int end_y, \
+                         ubyte r, ubyte g, ubyte b)
+{
+    ubyte *pfb, a = 0xFF;
+    int color = vinfo.bits_per_pixel/8.;
+    if(end_x == 0) end_x = vinfo.xres;
+    if(end_y == 0) end_y = vinfo.yres;
+
+    /* mmap() 함수를 이용해서 메모리맵을 작성한다. */
+    pfb = (ubyte *)mmap(0, vinfo.xres * vinfo.yres * color, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    /* 2개의 for 루프를 이용해서 면을 그린다. */
+    for(int x = start_x; x < end_x*color; x+=color) {
+        for(int y = start_y; y < end_y; y++) {
+            // *(pfb + (x+0) + y*vinfo.xres*color) = b;
+            // *(pfb + (x+1) + y*vinfo.xres*color) = g;
+            // *(pfb + (x+2) + y*vinfo.xres*color) = r;
+            unsigned short pixel;
+            pixel = makepixel(r, g, b);
+            * (unsigned short *) (pfb + x + y * vinfo.xres * color) = pixel;
+            *(pfb + (x+3) + y*vinfo.xres*color) = a;
+        }
+    }
+
+    /* 사용이 끝난 메모리 매핑을 해제한다. */
+    munmap(pfb, vinfo.xres * vinfo.yres * color);
+}
 
 int main(int argc, char **argv) {
     int fbfd, status, offset;
@@ -61,6 +89,8 @@ int main(int argc, char **argv) {
     drawpoint(fbfd, 50, 50, 255, 0, 0);
     drawpoint(fbfd, 100, 100, 0, 255, 0);
     drawpoint(fbfd, 150, 150, 0, 0, 255);
-    drawface(fbfd, 0, 0, 200, 100, 255, 0, 0);
+    drawface(fbfd, 0, 0, 0, 0, 255, 0, 0);
+    drawfacemmap(fbfd, 0, 0, 0, 0, 0, 255, 0);
+
 
 }
