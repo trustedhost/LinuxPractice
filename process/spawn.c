3fd1a40c95fe93d3817d0345225dcacc6a9ebcaf
trustedhost <mrchjung@korea.ac.kr>
2024-09-12 11:19:45 +0900

    posix_spawn 함수의 속성을 이용해서,wait 없이 비동기적으로 처리할 수 있다.

--- process/spawn.c
+++ process/spawn.c
@@ -7,9 +7,17 @@ extern char **environ;
 int system(char* cmd) {
     pid_t pid;
     int status;
+
+    posix_spawn_file_actions_t actions;
+    posix_spawnattr_t attrs;    
     char *argv[] = {"/bin/sh", "-c", cmd, NULL};
-    posix_spawn(&pid, "/bin/sh", NULL, NULL, argv, environ);
-    waitpid(pid, &status, 0);
+
+    posix_spawn_file_actions_init(&actions);
+    posix_spawnattr_init(&attrs);
+    posix_spawnattr_setflags(&attrs, POSIX_SPAWN_SETSCHEDULER);
+
+    posix_spawn(&pid, "/bin/sh", &actions, &attrs, argv, environ);
+    // waitpid(pid, &status, 0);
 
     return status;
 }
